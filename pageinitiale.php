3c13f2a3027f4835c31b40a2c6f49dba122719c7
jihanemastane <mastanejihane0@gmail.com>
2017-07-18 18:21:03 +0100

    Update pageinitiale.php

--- pageinitiale.php
+++ pageinitiale.php
@@ -1,1 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>salam</title>
+	<style type="text/css">
+		*{
+			margin-top: 50px;
+		}
+		a{
+			font-size: 20pt;
+			margin-top: 50;
+			display: block;
+			color: #2F4F4F;
+			text-align: center;
 
+
+		}
+		p:hover{
+			
+			opacity: 0.3;
+		}
+		h1{
+			text-align: center;
+			font-family: verdana;
+			color: grey;
+		}
+		.divi{
+			margin-left: 600px;
+			width: 300px;
+			height: 300px;
+			background-color: #F0E68C;
+			border-radius: 23%;
+		}
+		body{
+			background-color: #FFFACD;
+		}
+		
+	</style>
+</head>
+
+<body>
+<h1>Association ENSA MARRAKECH</h1>
+<div class="divi">
+<a href="http://localhost/ajoutmembre.php"><p>Ajouter un membre</p></a></br>
+<a href="http://localhost/suppressionmembre.php"><p>Supprimer un membre</p></a></br>
+<a href="http://localhost/affichermembre.php"><p>Rechercher un membre</p></a>
+</div>
+
+</body>
+</html>
