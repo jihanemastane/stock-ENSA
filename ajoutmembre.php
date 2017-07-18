b1d60d089786f1197c8d4255897e26013215d25b
jihanemastane <mastanejihane0@gmail.com>
2017-07-18 18:17:01 +0100

    Update ajoutmembre.php

--- ajoutmembre.php
+++ ajoutmembre.php
@@ -1,1 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		*{
+			margin-top: 50px;
+		}
+		a{
+			font-size: 20pt;
+			margin-top: 50;
+			display: block;
+			color: #2F4F4F;
+			text-align: center;
+
+		}
+		p:hover{
+			opacity: 0.3;
+		}
+		h1{
+			text-align: center;
+			font-family: verdana;
+			color: grey;
+		}
+		.divi{
+			margin-left: 500px;
+			width: 600px;
+			height: 600px;
+			background-color: #F0E68C;
+			border-radius: 20%;
+		}
+		body{
+			background-color: #FFFACD;
+		}
+	</style>
+</head>
+<body>
+<div class="divi">
+<a href="http://localhost/bdd.php"><p>ajouter un diplomé</p></a>
+<a href="http://localhost/ajoutdiplome.php"><p>ajouter un diplome</p></a>
+<a href="http://localhost/ajouterorganisation.php"><p>ajouter une organisation</p></a>
+<a href="http://localhost/ajoutertelephone.php"><p>ajouter un telephone</p></a>
+<a href="http://localhost/ajoutersolicitation.php"><p>ajouter une solicitation</p></a>
+<a href="http://localhost/ajoutversement.php"><p>ajouter un versement</p></a>
+<a href="http://localhost/fichedesolicitation.php"><p>Remplir la fiche de solicitation d'un membre</p></a>
+</div>
+</body>
+</html>
 
